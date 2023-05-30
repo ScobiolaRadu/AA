efa033eaf6acd077477a54c33e952a48671e73b6
ScobiolaRadu <80578568+ScobiolaRadu@users.noreply.github.com>
2023-05-31 02:02:49 +0300

    finished

--- tema-alg-genetici.cpp
+++ tema-alg-genetici.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <bitset>
+#include <vector>
 
 using namespace std;
 
@@ -110,6 +111,7 @@ int main()
     }
 
     // e
+    std::vector<int> participating;
     g << "\nProbabilitatea de incrucisare " << probability_crossover / 100 << "\n";
     for (int i = 1; i <= pop; i++)
     {
@@ -121,11 +123,116 @@ int main()
         if (u < probability_crossover / 100)
         {
             g << "<" << probability_crossover / 100 << " participa";
+            participating.push_back(i);
         }
-
         g << endl;
     }
 
-    //f
-    
+    // f
+    g << endl;
+    g << std::endl;
+    for (int i = 0; i < participating.size() - 1; i += 2)
+    {
+        int c1 = participating[i];
+        int c2 = participating[i + 1];
+
+        std::bitset<32> bits1(*reinterpret_cast<unsigned *>(&selected[c1]));
+        std::bitset<32> bits2(*reinterpret_cast<unsigned *>(&selected[c2]));
+
+        int punct = rand() % (sizeof(float) * 8);
+
+        g << "Recombinare dintre cromozomul " << c1 << " cu cromozomul " << c2 << ":" << std::endl;
+        g << bits1 << " " << bits2 << " punct " << punct << std::endl;
+
+        std::bitset<32> combined;
+        for (int j = 0; j < punct; j++)
+        {
+            combined[j] = bits1[j];
+        }
+        for (int j = punct; j < 32; j++)
+        {
+            combined[j] = bits2[j - punct];
+        }
+
+        std::bitset<32> combined2;
+        for (int j = 0; j < punct; j++)
+        {
+            combined2[j] = bits2[j];
+        }
+        for (int j = punct; j < 32; j++)
+        {
+            combined2[j] = bits1[j - punct];
+        }
+
+        g << "Rezultat    " << combined << " " << combined2 << std::endl;
+
+        chromosomes[c1] = *reinterpret_cast<float *>(&combined);
+        chromosomes[c2] = *reinterpret_cast<float *>(&combined2);
+    }
+
+    // g
+    g << "\nDupa recombinare:\n";
+    for (int i = 1; i <= pop; i++)
+    {
+        unsigned int *binary = (unsigned int *)&chromosomes[i];
+        std::bitset<sizeof(float) * 8> bits(*binary);
+        g << i << ": " << bits << " x= " << chromosomes[i] << " ";
+        g << "f=" << func(chromosomes[i], a, b, c) << endl;
+    }
+
+    // h
+    g << "\nProbabilitatea de mutatie pentru fiecare gena " << probability_mutation / 100 << "\n";
+    g << "Au fost modificati cromozomii:\n";
+    for (int i = 1; i <= pop; i++)
+    {
+        if (static_cast<float>(rand()) / RAND_MAX < probability_mutation / 100)
+        {
+            int geneIndex = rand() % 32;
+            unsigned int *binary = (unsigned int *)&chromosomes[i];
+
+            std::bitset<32> bits(*binary);
+            bits.flip(geneIndex);
+            *binary = bits.to_ulong();
+
+            g << i << endl;
+        }
+    }
+
+    g << "Dupa mutatie:\n";
+    for (int i = 1; i <= pop; i++)
+    {
+        unsigned int *binary = (unsigned int *)&chromosomes[i];
+        std::bitset<32> bits(*binary);
+        g << i << ": " << bits << " x= " << chromosomes[i] << " ";
+        g << "f=" << func(chromosomes[i], a, b, c) << endl;
+    }
+
+    // i
+    std::vector<float> maxFitnessHistory;
+    std::vector<float> meanFitnessHistory;
+    float maxFitness = 0.0;
+    float sumFitness = 0.0;
+    for (int i = 1; i <= pop; i++)
+    {
+        float fitness = func(chromosomes[i], a, b, c);
+        if (fitness >= maxFitness)
+        {
+            maxFitness = fitness;
+        }
+        maxFitnessHistory.push_back(maxFitness);
+        sumFitness += fitness;
+        float meanFitness = sumFitness / pop;
+        meanFitnessHistory.push_back(meanFitness);
+    }
+
+    g << "\nEvolutia maximului\n";
+    for (int i = 0; i < maxFitnessHistory.size(); i++)
+    {
+        g << maxFitnessHistory[i] << endl;
+    }
+    g << "\nMedia fitnessului\n";
+    for (int i = 0; i < meanFitnessHistory.size(); i++)
+    {
+        g << meanFitnessHistory[i] << endl;
+    }
 }
